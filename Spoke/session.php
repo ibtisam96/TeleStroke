3011fbeba0238d8f1680c37ee10bd1d54b6b58dc
norakhaled <nura.g@live.com>
2018-02-25 16:40:28 +0300

    Add files via upload

---
+++ Spoke/session.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>  
+
+<html itemscope itemtype="http://schema.org/Product" prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/html">
+    <head>
+          <link rel="stylesheet" type="text/css" href="../Telestroke.css">
+        <style type="text/css"> td {background: #c9c3c5}</style>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    </head>
+    <body onload="init()">
+
+        <header>
+<div id="logocontainer">
+    <img id="logo" src="../images/Logo.png">
+    <img id="helpbutton" src="../images/help.png" align="right">
+</div>
+
+ <ul class="navigate" id="navigation">
+            <li><a href="home.html">Home</a></li>
+            <li><a href="requestsession.php">Request Session </a></li>
+            <li><a href="Patients.php">Patients</a></li>
+            <li><a href="help.php">Help</a></li>                
+      </ul>
+</header>
+
+
+
+<table width="100%">
+  <tr><td>
+<div id="video" align="center" style="width: 50%">
+
+</div>
+</td>
+<td>
+<div align="right" style="width: 50%">
+HIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiHIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <br>HIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <br>HI <br>HI <br>HI <br>HI <br>HI <br>
+</div>
+</td>
+</tr>
+</table>
+         <script src="https://meet.jit.si/external_api.js"></script>
+        <script>
+            var domain = "meet.jit.si";
+            var options = {
+                roomName: "Hospital",
+                width: 700,
+                height: 700,
+                parentNode:document.getElementById("video"),
+                interfaceConfigOverwrite: {filmStripOnly: true},
+
+                configOverwrite: {},
+                
+            }
+            var api = new JitsiMeetExternalAPI(domain, options);
+        </script>
+</script>
+</html>

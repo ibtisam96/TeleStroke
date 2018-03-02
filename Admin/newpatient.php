7a15cb3b64bc72cf5ed4bc1a671093f6bc9840db
asma56 <35924575+asma56@users.noreply.github.com>
2018-03-03 01:10:09 +0300

    Update newpatient.php

--- Admin/newpatient.php
+++ Admin/newpatient.php
@@ -3,9 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Telestroke";
-
 $idform=$_POST['paitentid'];
-$nameform=$_POST['name'];
+$nameform=$_POST['name1'];
+$nameform1=$_POST['name2'];
 $phoneno=$_POST['phoneno'];
 $phoneno1=$_POST['phoneno1'];
 $email=$_POST['email'];
@@ -27,8 +27,8 @@ $checkstring="SELECT patient_id FROM patient WHERE patient_id = '$idform'";
 $checkResult=mysqli_query($conn,$checkstring)
 Or die ("<p>Unable to access your information <br> Please try again later or contact the admin</p>");
 if(mysqli_num_rows($checkResult) == 0){
-$sql = "INSERT INTO patient VALUES ('$idform','$nameform', '$phoneno','$phoneno1', '$email', '$genderform','$BDpaitent','$city' ,'$address1' ,'$address2','$hospitalid')";
-
+$sql = "INSERT INTO patient VALUES ('$idform','$nameform','$nameform1','$phoneno','$phoneno1', '$email', '$genderform',
+'$BDpaitent','$city' ,'$address1' ,'$address2','$hospitalid')";
 
 if(mysqli_query($conn, $sql)) { echo "<br><br><br><div style=' display: table; margin-right: auto; margin-left: auto;
  background-color:#eaeaea; radius:20px;'><p style='text-align:center;'>".$nameform." details were added successfully</p></div>";
